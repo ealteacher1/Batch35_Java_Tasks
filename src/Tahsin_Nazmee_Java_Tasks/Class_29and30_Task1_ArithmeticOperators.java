87c52d4be3e39440dc1e672823c884b92486a79d
Tahsin Nazmee <tnazmee@yahoo.com>
2024-04-23 13:53:57 -0500

    Completed Class 29 Task 1 (Practice each arithmetic operators 5 times) on 3/28/2024.

---
+++ src/Tahsin_Nazmee_Java_Tasks/Class_29and30_Task1_ArithmeticOperators.java
@@ -0,0 +1,135 @@
+package Tahsin_Nazmee_Java_Tasks;
+
+public class Class_29and30_Task1_ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// Arithmetic Operators (+, -, *, /, %)
+		// Assigning int 
+		int a = 6000;
+		int j = 60;		
+		
+		int a2 = 555;
+		int j2 = 11;
+		
+		int a3 = 63;
+		int j3 = 7;
+		
+		int a4 = 36;
+		int j4 = 6;
+		
+		int a5 = 42;
+		int j5 = 2; 
+		
+		// Addition 
+		int addition = a+j;
+		String aj_add = "The addition of a and j: ";
+		System.out.println("The addition of a and j: "+(a+j));
+		System.out.println(aj_add+addition);
+		
+		int addition2= a2+j2;
+		String aj_add2 = "The addition of a2 and j2: ";
+		System.out.println(aj_add2+addition2);
+		
+		int addition3= a3+j3;
+		String aj_add3 = "The addition of a3 and j3: ";
+		System.out.println(aj_add3+addition3);
+		
+		int addition4= a4+j4;
+		String aj_add4 = "The addition of a4 and j4: ";
+		System.out.println(aj_add4+addition4);
+		
+		int addition5= a5+j5;
+		String aj_add5 = "The addition of a5 and j5: ";
+		System.out.println(aj_add5+addition5);
+				
+		// Subtraction
+		int subtraction = a-j;
+		String aj_minus = "The subtraction of a and j: ";
+		System.out.println("The subtraction of a and j: "+(a-j));
+		System.out.println(aj_minus+subtraction);
+		
+		int subtraction2 = a2-j2;
+		String aj_minus2 = "The subtraction of a2 and j2: ";
+		System.out.println(aj_minus2+subtraction2);
+		
+		int subtraction3 = a3-j3;
+		String aj_minus3 = "The subtraction of a3 and j3: ";
+		System.out.println(aj_minus3+subtraction3);
+		
+		int subtraction4 = a4-j4;
+		String aj_minus4 = "The subtraction of a4 and j4: ";
+		System.out.println(aj_minus4+subtraction4);
+		
+		int subtraction5 = a5-j5;
+		String aj_minus5 = "The subtraction of a5 and j5: ";
+		System.out.println(aj_minus5+subtraction5);
+				
+		// Multiplication
+		int multiplication = a*j;
+		String aj_multiply = "The multiplication of a and j: ";
+		System.out.println("The multiplication of a and j: "+(a*j));
+		System.out.println(aj_multiply+multiplication);
+		
+		int multiplication2 = a2*j2;
+		String aj_multiply2 = "The multiplication of a2 and j2: ";
+		System.out.println(aj_multiply2+multiplication2);
+		
+		int multiplication3 = a3*j3;
+		String aj_multiply3 = "The multiplication of a3 and j3: ";
+		System.out.println(aj_multiply3+multiplication3);
+		
+		int multiplication4 = a4*j4;
+		String aj_multiply4 = "The multiplication of a4 and j4: ";
+		System.out.println(aj_multiply4+multiplication4);
+		
+		int multiplication5 = a5*j5;
+		String aj_multiply5 = "The multiplication of a5 and j5: ";
+		System.out.println(aj_multiply5+multiplication5);
+				
+		// Division
+		int division = a/j;
+		String aj_divide = "The division of a and j: ";
+		System.out.println("The division of a and j: "+(a/j));
+		System.out.println(aj_divide+division);
+		
+		int division2 = a2/j2;
+		String aj_divide2 = "The division of a2 and j2: ";
+		System.out.println(aj_divide2+division2);
+		
+		int division3 = a3/j3;
+		String aj_divide3 = "The division of a3 and j3: ";
+		System.out.println(aj_divide3+division3);
+		
+		int division4 = a4/j4;
+		String aj_divide4 = "The division of a4 and j4: ";
+		System.out.println(aj_divide4+division4);
+		
+		int division5 = a5/j5;
+		String aj_divide5 = "The division of a5 and j5: ";
+		System.out.println(aj_divide5+division5);
+				
+		// Remainder
+		int remainder = j%a;
+		String aj_remainder = "The remainder of a and j: ";
+		System.out.println("The remainder of a and j: "+(j%a));
+		System.out.println(aj_remainder+remainder);
+		
+		int remainder2 = j2%a2;
+		String aj_remainder2 = "The remainder of a2 and j2: ";
+		System.out.println(aj_remainder2+remainder2);
+		
+		int remainder3 = j3%a3;
+		String aj_remainder3 = "The remainder of a3 and j3: ";
+		System.out.println(aj_remainder3+remainder3);
+		
+		int remainder4 = j4%a4;
+		String aj_remainder4 = "The remainder of a4 and j4: ";
+		System.out.println(aj_remainder4+remainder4);
+		
+		int remainder5 = j5%a5;
+		String aj_remainder5 = "The remainder of a5 and j5: ";
+		System.out.println(aj_remainder5+remainder5);
+		
+		/* End */
+	}
+}
