1eb6de1c49d55a5eedb3225f3b120b33300ccf16
syfur <syfur@10.0.0.136>
2024-04-23 01:20:55 -0400

    Class-03 : Task-3,Done By Israt On - 04/23/2024.

---
+++ src/israts_java_task/Class_3_Task_3_Array.java
@@ -0,0 +1,35 @@
+package israts_java_task;
+
+public class Class_3_Task_3_Array {
+
+	public static void main(String[] args) {
+		
+// Task - 03--
+		String Rain []= new String [5];
+		Rain [0]= "Rose";
+		Rain [1]= "Is";
+		Rain [2]= "Red";
+		Rain [3]= "As";
+		Rain [4]= "Blood";
+	for (int i=0; i <=Rain.length-1;i++) {
+		System.out.println(" The Value of String : " +Rain[i]);
+	}
+//Print the value of Index number 2 and 3 individually--
+	System.out.println("The value of Index 1: " +Rain[1]);
+	System.out.println("The value of Index 2 : "+Rain[2]);
+   
+	
+// Print all the value from 1st to last index--
+	for (int i=0;i<=Rain.length-1;i++) {
+		System.out.println("The value of string : "+ Rain[i]);
+	}
+// Print all the value from last index to first index --
+	for (int i=Rain.length-1;i>=0;i--) {
+		System.out.println("The value of string : "+ Rain[i]);
+	}
+// Print all the value from 5th to 2nd index--
+	for (int i=4;i>=2;i--) {
+		System.out.println("The value of string "+ Rain[i]);
+	}
+}
+}
