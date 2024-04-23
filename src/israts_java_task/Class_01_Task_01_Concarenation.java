337b5c6f33a2d0c68878497fd070f08dbffd306f
syfur <syfur@10.0.0.136>
2024-04-23 17:24:18 -0400

    Class-01:Task-1 Completed By Israt On 04/23/2024

---
+++ src/israts_java_task/Class_01_Task_01_Concarenation.java
@@ -0,0 +1,65 @@
+package israts_java_task;
+
+public class Class_01_Task_01_Concarenation {
+
+	public static void main(String[] args) {
+//Task-1--	CONCATINATIONS--	
+ // 1. int + int + int
+
+        int a1= 15;
+        int a2 = 10;
+        int a3 = 20;
+        System.out.println(a1 + a2 + a3);
+        
+// 2. int + String + int 
+        int a4 =11;
+        String a5 = " Life is beautiful  ";
+        int a6 = 10;
+        System.out.println(a4 + a5 + a6);
+        
+// 3. float + double + int
+        float a7= 9.5f;
+        double a8 = 7.2;
+        int a9 = 45;
+        System.out.println(a7 + a8 +a9);
+//4.String+Float+int 
+        String b1= "Israt";
+        float b2 =6.9f;
+        int b3= 50;
+        System.out.println(b1+b2+b3);
+//5.float + int + String
+        float b4 = 22.50f;
+        int b5 = 300;
+        String ros = "Lets enjoy the music  ";
+        System.out.println(b4 + b5 + ros);
+//6.print float + int + float + double-
+        float b6 = 20.5f;
+        int b7 = 96;
+        double b8 = 16.900; 
+        System.out.println(b6 + b7 + b8);
+// 7. String + int + double + String
+        String scenario = " Lets go to the beach ";
+        int c5 = 10;
+        double c6 = 79.9;
+        String mountain = " Lets climb on mount everest";
+
+        System.out.println(scenario + c5 + c6 + mountain );
+       
+// 8. String + int + String + double
+        String b9 = " Lets go to the movie with ";
+        int b0 = 200;
+        String b10 = " We will watch notebook ";
+        double b11 = 69.5;
+
+        System.out.println(b9 + b0 + b10 + b11);  
+// 9. int + int + String + (int + int + int)
+        int c1 = 50;
+        int c2 = 100;
+        int c3 = 150;
+        String easy = " That movie was pretty awesome! ";
+        int add = c1+c2+c3;
+        System.out.println(c1 +c2 + easy + add);
+       
+	}
+
+}
