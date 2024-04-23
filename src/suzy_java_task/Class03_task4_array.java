5d686b1e7d5001f0336b741fb917c86ee415faeb
suzy8 <suzy8@10.0.0.176>
2024-04-23 15:53:38 -0700

    class03_task4_done by suzy 04/23/2024

---
+++ src/suzy_java_task/Class03_task4_array.java
@@ -0,0 +1,60 @@
+package suzy_java_task;
+
+public class Class03_task4_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//	Array task 04 --
+		
+//create array without limiting the index length and put 5 int value --
+		int life []=new int [5]; 
+		
+		
+		
+//		print all the value with each for loop --
+		
+		for(int b=0;b<=0;b++) {
+			System.out.println("the value is : "+life[b]);
+			
+		}
+//			find out the length of array --
+			System.out.println(life.length);
+			
+			
+			
+//			print all the value from first index to last index using for loop --
+			for(int a=0;a<=life.length-1;a++) {
+				System.out.println("the value is :"+life[a]);
+				
+			}
+	
+				
+//		print all the value from last index to first index with for loop in reverse order  --
+				for(int a=life.length-1;a>=0;a-- ) {
+					System.out.println("the valus is "+life[a]);
+					
+					
+				}			
+//	print all the value 5th index to 2nd index --
+				
+		for(int a=4;a<=1;a++) {
+			System.out.println("the value is:"+life[4]);
+			
+			
+			
+				}
+				
+		}
+		
+		
+		
+
+		
+		
+		
+		
+	}
+
+
