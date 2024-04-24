8351a4e057caa0745681c30d36f34958bf3273ba
suzy8 <suzy8@10.0.0.176>
2024-04-23 17:01:22 -0700

    class01_task1_concatination_done by suzy 04/23/2024

---
+++ src/suzy_java_task/Class01_task1_concatination.java
@@ -0,0 +1,27 @@
+package suzy_java_task;
+
+public class Class01_task1_concatination {
+	
+
+	int s = computer + science1 + math1;
+    float r = s/4f;
+
+    if (r >= 90 && d<= 100) {
+        System.out.println("Student got grade A");
+
+    }
+    else if (r>= 80 && r <90) {
+        System.out.println("Student got grade B");
+    }
+    else if (r>= 70 && d < 80) {
+        System.out.println("Student got grade C");
+    }
+    else if (r >= 60 && d <70) {
+        System.out.println("Student got grade D");
+
+    }
+    else {
+        System.out.println("Student got Grade F");
+    }
+
+}
