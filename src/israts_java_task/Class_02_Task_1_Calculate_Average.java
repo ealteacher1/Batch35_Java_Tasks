c37ebf04e87672261bc79f6167c546fb7826d32c
syfur <syfur@10.0.0.136>
2024-04-25 16:23:48 -0400

    Class-2:Task-1_Calculate_Average,Updated By Israt On 04/25/2024

---
+++ src/israts_java_task/Class_02_Task_1_Calculate_Average.java
@@ -0,0 +1,19 @@
+package israts_java_task;
+
+public class Class_02_Task_1_Calculate_Average {
+
+	public static void main(String[] args) {
+//Task-1 CALCULATE AVERAGE--
+		  int math = 60;
+	      int science = 66;
+	      int physics = 48;
+	      int biology = 74;
+	      int b = math + science + physics + biology;
+	      System.out.println(b);
+	      float c=b/4f ;
+	      System.out.println("The average of subject " +c);
+	      
+
+	}
+
+}
