6e72d7d97bd0b9d02870d5a9ee44f10d18e63584
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-26 16:30:40 -0400

    Task1-switchCase

---
+++ src/tasks_shaheena/SwitchCase.java
@@ -0,0 +1,30 @@
+package tasks_shaheena;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		
+		char ch ='a';
+		
+		switch(ch) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u': 
+			
+		System.out.println( ch  +   "  is vowel");	
+		
+		break;
+			default:
+				System.out.println(ch +"  it's a Number");
+		}		
+		
+		
+	}
+		
+		
+
+	}
+
+
