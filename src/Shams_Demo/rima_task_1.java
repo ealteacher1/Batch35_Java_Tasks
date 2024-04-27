709ad5f8cf796394b39990ae132f763ed9c1e23a
User <User@DESKTOP-M2EEJ08.lan>
2024-04-27 09:28:54 -0600

    class1 tasks completed on _4/27/24

---
+++ src/Shams_Demo/rima_task_1.java
@@ -0,0 +1,62 @@
+package Shams_Demo;
+
+public class rima_task_1 {
+
+	public static void main(String[] args) {
+		// Concatenate and Print.
+		
+		int a=1;
+		System.out.println(a);
+		
+		int b=2;
+		System.out.println(b);
+		
+		int c =3;
+		System.out.println(c);
+		
+		float f=4.4f;
+		System.out.println(f);
+		
+		double d=44.44;
+		System.out.println(d);
+		
+		String S="this is rima";
+		System.out.println(S);
+		
+		// int+int+int.
+		System.out.println(a+b+c);
+		
+		//int+String+int.
+		System.out.println(a+S+b);
+		
+		//float+int+double.
+		System.out.println(f+a+d);
+		
+		//String+float+int.
+		System.out.println(S+f+a);
+		
+		//float+int+String.
+		System.out.println(f+a+S);
+		
+		//printfloat+int+float+double.
+		System.out.println(f+a+f+d);
+		
+		//String=int+double+String.
+		System.out.println(S+a+d+S);
+		
+		//String+int+String+doubl.
+		System.out.println(S+a+S+d);
+		
+		//int+int+String+(int+int+int).
+		System.out.println(a+b+S+(a+b+c));
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
