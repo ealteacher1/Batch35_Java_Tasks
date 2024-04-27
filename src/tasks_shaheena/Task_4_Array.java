fa29d52ea6a2b491c2a74fe7326e28ccdfb68b39
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-27 14:55:59 -0400

    Task_4_Array 4-27-2024

---
+++ src/tasks_shaheena/Task_4_Array.java
@@ -0,0 +1,75 @@
+package tasks_shaheena;
+
+public class Task_4_Array {
+
+	public static void main(String[] args) {
+		
+
+		/* Create an int array without limiting the index length and Put 5 int Value there.
+		 * Print all the value with for each loop */
+		
+		
+	   int   Marks  [] = { 90, 65, 70, 80, 85, };
+		
+		System.out.println("The length of array is: "  + Marks.length);   // length of array 
+		
+		for(int x: Marks){
+		System.out.println(x);  //prints values using for each loop.
+		 
+		}}} 
+
+		
+		/* Find  the length of the array and 
+		 * Print all the value from 1rst index to last index with For loop.*/
+		
+		
+		
+	   /*   int number  [] = { 19, 28,  45, 38, 50, };
+		   System.out.println("length of array is : " + number.length);
+		
+		
+		   for(int n=0; n<=number.length-1; n++) {
+			
+			System.out.println(number[n]);
+		
+		
+	     }}}  */
+		
+		
+	      /*Print all the value from last Index (a.length-1) to first Index
+		  *  (Printing should be in reverse order)*/
+		
+		
+	/*	int Score [] ={20, 30, 40, 50, 60, };
+		
+		for (int  S =Score.length-1;  S>=0; S--) {    //Prints in reverse -forloop 
+		System.out.println(Score[S]);	
+		
+		} 
+		}}*/
+
+
+
+
+		 
+		
+		// *Print all the value from 5th index to 2nd index.
+		
+		
+	/*	int number[] = {1, 3, 5, 7, 9, };
+		
+		for (int a=4; a>=1; a--) {
+		System.out.println("5th to 2nd values are: "  + number[a]);	  //prints 5th to 2nd index values
+		}
+		}}*/
+		
+		
+		 	
+		
+		
+		
+		
+
+	
+
+
