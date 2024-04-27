3a618d1127fd3af2e9ce89c36f3a0439b20a0a09
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-27 13:43:49 -0400

    Task_3_Array 4-27=2024

---
+++ src/tasks_shaheena/Task_3_Array.java
@@ -0,0 +1,81 @@
+package tasks_shaheena;
+
+public class Task_3_Array {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		/*Create an String array without limiting the index length (Variable Name should not be single Digit,
+		  It can be a name or anything but not a,b,c,d) and Put 5 String Value there. 
+		  Print all the value with for each loop.*/
+		 
+		
+		 String Cars [] = {"Lexus", "Mercedes", "Volvo", "Audi", "BMW" };
+		
+		   System.out.println("the length of array : "  +  Cars.length);  // prints the length of Array
+		
+		   for(String d: Cars) {
+			 System.out.println(d);
+		     }}} 
+		
+		
+		 /* Print the value of index number 2 and index 3 individually.
+		    (Be careful with the index name.IndexName and Index number are not same.)*/
+		
+		
+		/* String Cars [] = {"Lexus", "Mercedes", "Volvo", "Audi", "BMW" };
+		
+		 
+		 System.out.println(Cars[2]);   
+		 System.out.println(Cars[3]);
+	}} */
+		 
+		 
+		 // Print all the value from 1rst index to last index with For loop 
+		
+	/*	
+		String Cars [] = {"Lexus", "Mercedes", "Volvo", "Audi", "BMW" };
+		
+		for (int  C = 0;  C<=Cars.length-1; C++){ 
+		  
+			 System.out.println(Cars[C]);
+		}
+		
+		 
+	}}*/
+		
+		
+		
+		
+		  
+		 // Print all the value from last Index (a.length-1) to first Index (Printing should be in reverse order)
+		
+		
+       /*  String Cars [] = {"Lexus", "Mercedes", "Volvo", "Audi", "BMW" };
+		
+		    for (int  C= Cars.length-1;  C>=0; C--){   //Prints in reverse oprder 
+		  
+			 System.out.println(Cars[C]);
+		
+		    }}}  */
+		  
+		
+
+      // Print all the value 5th index to 2nd index.
+		 
+
+       /*  String Cars [] = {"Lexus", "Mercedes", "Volvo", "Audi", "BMW" };
+	
+         for (int  C=4;  C>=2; C--){   //Prints 5th index to 2nd index , in reverse order  
+   		  
+			 System.out.println(Cars[C]);
+			 
+         }}}*/
+
+
+
+	
+
+
