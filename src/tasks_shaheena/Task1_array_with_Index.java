1c665d2ffa6d390c9feddf8ba75c15e3410c1b33
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-27 11:19:25 -0400

    Task_2 -array

--- src/tasks_shaheena/Task1_array_with_Index.java
+++ src/tasks_shaheena/Task1_array_with_Index.java
@@ -144,4 +144,4 @@ public class Task1_array_with_Index {
           
 	} */
 
-  
+	}}
---
+++ src/tasks_shaheena/Task_2_Array.java
@@ -0,0 +1,162 @@
+package tasks_shaheena;
+
+public class Task_2_Array {
+
+	public static void main(String[] args) {
+		
+		
+		/*  Create an String array with 11 index (VariableNameshouldnotbesingleDigit,
+		 *  it can be a name or anything but not a,b,c,d) and Put any value on Index number1(ex:a[0]),
+		 *  index number3 ,and Index number 6.
+		 *  Find out the length of the array and Print all the value with for each loop */
+		
+    	String State [] = new String [11];
+		
+		State[0]="New York";
+		State[1]="Michigan";
+		State[2]="Chicago";
+		State[3]="Pensylvania";
+		State[4]="Connecticut";
+		State[5]="New jersey";
+		State[6]="California";
+		State[7]="Texas";
+		State[8]="Arizona";
+		State[9]="Maine";
+		State[10]="Florida";
+		
+		System.out.println("The length of array is: "  + State.length);
+	
+		
+		
+		for(String  x : State ) {  
+		System.out.println(x);   // prints all the values with for each loop
+		}} }
+		
+		
+		
+	 
+		// *  Print the value of index number 3 and index 5 individually.
+		
+		/*String city[] = new String[11];
+		
+		city[0]="Flushing";
+		city[1]="Novi";
+		city[2]="hoboken";
+		city[3]="Sterling Heights";
+		city[4]="Astoria";
+		city[5]="Staten Island";
+		
+		System.out.println(city[2]);  // Prints index # 3
+		System.out.println(city[4]);   // Prints index # 5 
+		
+		
+	}}*/
+		
+		
+		
+   // *  Find out the length of the array and Print all the value from 1rst index to last index with For loop
+         //prints all the values 
+		 
+	/*String State[]= new String[11];	 
+	
+	    State[0]="New York";
+		State[1]="Michigan";
+		State[2]="Chicago";
+		State[3]="Pensylvania";
+		State[4]="Connecticut";
+		State[5]="New jersey";
+		State[6]="California";
+		State[7]="Texas";
+		State[8]="Arizona";
+		State[9]="Maine";
+		State[10]="Florida";
+		
+	
+		System.out.println(State.length);   // prints length of the array 
+		
+		for(int x=0; x<=State.length-1; x++) {  
+			System.out.println(State[x]); // prints all values 
+			
+		}}}*/
+		
+	
+		   
+		// *  Print all the value from lastIndex(a.length-1) to first Index (Printing should be in reverse order)
+		
+		
+		/*String State[]= new String[11];
+		
+		    State[0]="New York";
+			State[1]="Michigan";
+			State[2]="Chicago";
+			State[3]="Pensylvania";
+			State[4]="Connecticut";
+			State[5]="New jersey";
+			State[6]="California";
+			State[7]="Texas";
+			State[8]="Arizona";
+			State[9]="Maine";
+			State[10]="Florida";
+			
+			for(int x=State.length-1; x>=0; x--) {
+				
+			System.out.println(State[x]);}   //prints all the values from last to first index , in reverse order
+			}}*/
+		
+		
+		
+		
+		
+		
+	  // * Print all the value 5th index to 8thindex.
+		
+	/*	String State[]= new String[11];
+		
+	    State[0]="New York";
+		State[1]="Michigan";
+		State[2]="Chicago";
+		State[3]="Pensylvania";
+		State[4]="Connecticut";
+		State[5]="New jersey";
+		State[6]="California";
+		State[7]="Texas";
+		State[8]="Arizona";
+		State[9]="Maine";
+		State[10]="Florida";
+		
+		for (int x=3; x<=7; x++){ 
+		{System.out.println(State[x]);  // Prints 4th to 8th index value 
+		
+		}}}}*/
+		  
+		
+		
+		// * Print all the value from 8th Index to 4th index.*/
+         
+		
+	/*	String State[]= new String[11];
+		
+	    State[0]="New York";
+		State[1]="Michigan";
+		State[2]="Chicago";
+		State[3]="Pensylvania";
+		State[4]="Connecticut";
+		State[5]="New jersey";
+		State[6]="California";
+		State[7]="Texas";
+		State[8]="Arizona";
+		State[9]="Maine";
+		State[10]="Florida";
+		
+		for (int x=7; x>=3; x--){ 
+			{System.out.println(State[x]);   // prints value of 8th index to 4 th index 
+			 
+			}}}}*/
+
+
+
+
+
+	
+
+
