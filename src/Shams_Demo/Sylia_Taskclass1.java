cb2d36fa8ebff2d81b94a7bdcabc15a93145191c
Personal <Personal@DESKTOP-VGQMF2U>
2024-04-27 16:37:27 -0600

    task1_2 completed on 4/26/2024

--- src/Shams_Demo/Sylia_Taskclass1.java
+++ src/Shams_Demo/Sylia_Taskclass1.java
@@ -4,7 +4,21 @@ public class Sylia_Taskclass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a=70;
+		int b=25;
+		int c=90;
+		System.out.println(a+b+c);
+		String s="ramadan mobarak";
+		System.out.println(a+s+c);	
+		float f=0.9f;
+		double d=2.9d;
+		System.out.println(f+d+b);
+		System.out.println(s+f+c);
+		System.out.println(s+a+f);
+		System.out.println(f+a+f+d);
+		System.out.println(s+c+d+s);
+		System.out.println(s+b+s+d);
+		System.out.println(a+b+s+(a+c+b));	
 	}
 
 }
--- src/Shams_Demo/Sylia_Taskclass2.java
+++ src/Shams_Demo/Sylia_Taskclass2.java
@@ -4,7 +4,85 @@ public class Sylia_Taskclass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// taske01
+		int m= 2;
+		while (m>=-10) {
+			System.out.println("m is "+m);
+			m--;
+		}
+		int n=3;
+		while (n<=20) {
+			System.out.println("n is"+n);
+			n++;
+		}
+		int p= -355;
+		do {
+			System.out.println("value of p is ="+p);
+					p++;
+	
 	}
+		while (p<= -255);
+		
+		int h=-956;
+		do {
+			System.out.println("value of h="+h);
+			h--;
+		}
+		while(h>= -960);
+	
+		{
+		int i= -10;
+		while (i>=10) {
+			System.out.println("i=to"+i);
+		i++;
+		}
+		 int a= -5;
+		 while (a<=5) {
+			 System.out.println("a="+a);
+	     if (a==3) {
+	    	 System.out.println("sylia");
+				 break;
+			 }
+			 a++;
+	 }
+	for (int s= -500; s<= -200; s++) {
+		if (s==-278) {
+			break;
+	}
+	System.out.println("s ="+s);
+	}
+	
+	int a= -5;
+	 while (a<=5) {
+		 System.out.println("a="+a);
+    if (a==3) {
+   	 System.out.println("sylia");
+			 break;
+		 }
+		 a++;
+	 }
+	 
+	 
 
-}
+	//Task Calculate average
+//	int math=60;
+//	int science=66;
+//	int physics=48;
+//	int biologie=74;
+//	int sum= math+science+physics+biologie;
+//	System.out.println(sum);
+//	double average= sum/4;
+//    System.out.println(average);
+//	if (average<=100 && average >=80) {
+//		System.out.println("grade =A");
+//	}
+//	else if (average<=70 || average>=50) {
+//		System.out.println("grad=B");
+//	}
+//	else if (average<=20 && average>=15) {
+//		System.out.println("grade=C");
+//	}
+//	else {
+//		System.out.println("programe is succeful");
+//	}
+
