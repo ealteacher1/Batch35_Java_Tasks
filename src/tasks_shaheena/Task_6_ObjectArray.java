915f68b225450cabde6e1c534e0d5154c0b76af3
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-27 18:32:17 -0400

    Task_6_Object Array: 4-27-2024

---
+++ src/tasks_shaheena/Task_6_ObjectArray.java
@@ -0,0 +1,91 @@
+package tasks_shaheena;
+
+public class Task_6_ObjectArray {
+
+	public static void main(String[] args) {
+
+		/*Create an Object array without limiting the index length and Put 5 int Value there.
+		  * Print all the value with for each loop*/
+		
+		
+		
+		
+		Object actor  [] = {"Charlie Chaplin", "The Gold Rush", 1925, 'A', 4000000, };
+		
+		for (Object  a : actor) {   // prints all values using for each loop 
+			
+			System.out.println(a);
+		
+			}
+		
+		System.out.println("length of the array:"  + actor.length);
+		
+	      }} 
+		
+		
+		 // Find out the length of the array and Print all the value from 1rst index to last index with For loop  
+		 
+		
+	/*	Object actor  [] = {"Charlie Chaplin", "The Gold Rush", 1925, 'A', 4000000, };
+		
+		
+		for(int c=0; c<=actor.length-1; c++) {
+		System.out.println(actor[c]);	
+		 
+		
+		} } }  */
+		
+		
+		
+		
+		
+	 // Print all the value from last Index (a.length-1) to first Index (Printing should be in reverse order)
+		 
+        
+		    
+		/* Object actor  [] = {"Charlie Chaplin", "The Gold Rush", 1925, 'A', 4000000, };
+		
+		
+		    for(int c=actor.length-1; c>=0; c--) {
+		    System.out.println(actor[c]);	          // Prints in reverse
+		 
+		
+		} } }  */
+		
+		
+		
+		  /* Print the value of index number 2 and index 3 individually.
+		    (Be careful with the index name. Index Name and Index number are not same.)*/
+		
+		
+		  /*  Object actor  [] = {"Charlie Chaplin", "The Gold Rush", 1925, 'A', 4000000, };
+		  
+		
+		   System.out.println(actor[1]);
+		   System.out.println(actor[2]);
+		
+	        }}*/
+		
+		
+		
+		
+		 
+		 
+		 
+		 // Print all the value 5th index to 2nd index.
+		 
+
+		   /* Object actor  [] = {"Charlie Chaplin", "The Gold Rush", 1925, 'A', 4000000, };
+		    
+		    for (int  c =actor.length-1;  c>=1; c--) {
+		    	System.out.println(actor[c]);
+		    }
+		    }}*/
+	
+
+
+
+
+	
+
+
