3b66fd96c2ef12fac91fd6b328ff7e428b5ea6fa
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-27 16:38:56 -0400

    Task_5_ObjectArray:  4-27-2024

---
+++ src/tasks_shaheena/Task_5_ObjectArray.java
@@ -0,0 +1,158 @@
+package tasks_shaheena;
+
+public class Task_5_ObjectArray {
+
+	public static void main(String[] args) {
+		 
+		Object myaccount[]= new Object[10];
+		  
+		  myaccount[0]="Shaheena";    // index # 1
+		  myaccount[1]="ChaseBank";
+		  myaccount[2]="Checking";
+		  myaccount[3]="Ac12346789";
+		  myaccount[4]=2.5;            //index # 5
+		  myaccount[5]="CD";
+		  myaccount[6]=3.75;
+		  myaccount[7]="Savings";     // index # 8 
+		  myaccount[8]=1.5;
+		  myaccount[9]="College Point";
+		  
+		  System.out.println("length of the Object array:" +  myaccount.length);
+		  
+		 
+		  for( Object x: myaccount) {  
+			  
+			  System.out.println(x);    //prints all values -for each loop 
+		 
+		  }}}   
+		 
+
+		 
+		   
+		  //  Find out the length of the array and Print all the value from 1rst index to last index with For loop.
+		
+		
+	/*	Object Players [] = new Object[10];
+		
+		Players[0]="Nadal";
+		Players[1]="Federer";
+		Players[2]="Djokovich";  
+		Players[3]="Mbappe";   
+		Players[4]="Messy";    
+		Players[5]="Pele";   
+		Players[6]="Md.Ali";   
+		Players[7]="Kareem A jabbar";   
+		Players[8]="Shaquille o'Neal";   
+		Players[9]="Neymar";   
+		    
+		
+		System.out.println("Array legnth is: "  + Players.length);
+		
+		for (int p =0; p<=Players.length-1; p++){
+			System.out.println(Players[p]);
+			
+			
+		}}} */
+		    
+		    
+	
+		 /* Print the value of index number 2 and index 3 individually.
+		 * (Be careful with the index name. Index Name and Index number are not same.)
+		 */
+		
+	/*	Object Players [] = new Object[10]; 
+		
+		
+		Players[0]="Nadal";
+		Players[1]="Federer";      // index 2
+		Players[2]="Djokovich";   // index 3  
+		Players[3]="Mbappe";   
+		Players[4]="Messy";    
+		Players[5]="Pele";   
+		Players[6]="Md.Ali";   
+		Players[7]="Kareem A jabbar";   
+		Players[8]="Shaquille o'Neal";   
+		Players[9]="Neymar";   
+		    
+		System.out.println(Players[1]);
+		System.out.println(Players[2]);
+	}}  */
+	
+		
+		// Print all the value from last Index to first Index (Printing should be in reverse order)
+		  
+		
+			
+    /* Object Players [] = new Object[10]; 
+		
+		
+		Players[0]="Nadal";
+		Players[1]="Federer";      
+		Players[2]="Djokovich";    
+		Players[3]="Mbappe";   
+		Players[4]="Messy";    
+		Players[5]="Pele";   
+		Players[6]="Md.Ali";   
+		Players[7]="Kareem A jabbar";   
+		Players[8]="Shaquille o'Neal";   
+		Players[9]="Neymar"; 
+		
+		for (int p = Players.length-1; p>=0; p--) {
+		System.out.println(Players[p]); 	
+		}
+	      }
+           }*/
+	
+		// Print all the value 5th index to 8th index.
+      
+		
+	/*	Object Players [] = new Object[10]; 
+		
+		
+		Players[0]="Nadal";
+		Players[1]="Federer";      
+		Players[2]="Djokovich";    
+		Players[3]="Mbappe";   
+		Players[4]="Messy";    
+		Players[5]="Pele";   
+		Players[6]="Md.Ali";   
+		Players[7]="Kareem A jabbar";   
+		Players[8]="Shaquille o'Neal";   
+		Players[9]="Neymar";   
+        
+		
+		for (int p=3; p<=7; p++) {
+        
+   	   
+       System.out.println("The Value of index : " + Players[p]);
+         }}}*/
+         
+		 
+		
+		// Print all the value from 8th Index to 4th index.
+     
+		
+	/*	Object Players [] = new Object[10]; 
+		
+		
+		Players[0]="Nadal";
+		Players[1]="Federer";      
+		Players[2]="Djokovich";    
+		Players[3]="Mbappe";   
+		Players[4]="Messy";    
+		Players[5]="Pele";   
+		Players[6]="Md.Ali";   
+		Players[7]="Kareem A jabbar";   
+		Players[8]="Shaquille o'Neal";   
+		Players[9]="Neymar"; 
+
+		 for (int p=7; p>=3; p--) {
+			 
+	     System.out.println("The Value of index : " + Players[p]);
+
+		 }}}*/
+
+
+	
+
+
