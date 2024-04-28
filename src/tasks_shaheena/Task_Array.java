d85fac3fd753df362384c3e6fb7d20c0f380f910
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-04-28 17:06:51 -0400

    Task_Array 4-28-2024

---
+++ src/tasks_shaheena/Task_Array.java
@@ -0,0 +1,87 @@
+package tasks_shaheena;
+
+public class Task_Array {
+
+	public static void main(String[] args) {
+
+		//1.make 10 box Array: Input 3 box variables
+		
+		/* Print the value of index 5
+		 * Print the length of the array
+		 * Print all Box's values
+		 * Print all values of array with for loop
+		 */
+		
+		
+	     String   Singers []= new  String   [10];
+		
+		  Singers[0] ="Barbra Streisand";
+		  Singers[1] ="Celine Dione";
+		  Singers[2] ="Wilie Nelson";
+		  Singers[3] ="Adele Adkins";
+		  Singers[4] =" Ed seeran ";
+		  Singers[5] ="Lata Mangeskar";
+		  Singers[6] ="Mohammad Rafi";
+		  Singers[7] ="Mehedi Hassan";
+		  Singers[8] ="Noor Jahan";
+		  Singers[9] ="Judy Garland";
+			
+			
+				
+		  for (int i=0; i<=Singers.length-1; i++){
+				
+				System.out.println(( Singers[i]));
+		  }
+
+		System.out.println("Index # 5 is : "+Singers[4]);  //5th index 
+		 System.out.println("Length of Array: " + Singers.length);
+	
+	}}  
+		 
+	
+         //2. Make 3 box Array 		
+		// Make 7 box Array with long names (Varchar)
+		
+		/*
+	     String   Movies []= new String[3]; 
+	       
+	      Movies[0]="Titanic";
+	      Movies[1]="Roots";
+	      Movies[2]="Terminal";
+	      
+	      for (int j =0; j<Movies.length; j++) {
+	    	  
+	     System.out.println(Movies[j]);
+	      }
+	     System.out.println("Length of the array " +Movies.length); 
+	      
+	}}*/
+		
+		//Make 7 box Array with long names (Varchar)
+		
+		/*Object Password [] = new Object [7];
+		
+		Password[0]= "Rainbow99insky";
+		Password[1]=  "love4NY";
+        Password[2]=  "LoveBD1971";
+        Password[3]=  "Sunshine123&0 ";					
+        Password[4]=  "Niagra@Bufflo";
+        Password[5]=  "ShineLikeadIamond*";
+		Password[6]=  "EveninginParis!";
+		
+		
+		for (int k =0; k<Password.length; k++) {
+			System.out.println(Password[k]);
+		
+		}
+		System.out.println("Value of 5th Index :" +Password[4]);
+		System.out.println("Length of the Array : "+ Password.length);
+		
+	}}*/
+		
+		
+		
+
+	
+
+
