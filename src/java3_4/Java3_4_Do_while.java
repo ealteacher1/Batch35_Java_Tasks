e7fbe13cb0be071105852d2de4c42b14116264c3
User <User@DESKTOP-M2EEJ08.lan>
2024-04-28 00:21:28 -0600

    Java3_4_Do_while completed on 4/28/2024

---
+++ src/java3_4/Java3_4_Do_while.java
@@ -0,0 +1,17 @@
+package java3_4;
+
+public class Java3_4_Do_while {
+
+	public static void main(String[] args) {
+		// initialization.
+		int R=3;
+		do {
+			System.out.println("R="+R);
+			//increment part.
+			R++;
+		}
+		//condition.
+while(R<=20);
+	}
+
+}
