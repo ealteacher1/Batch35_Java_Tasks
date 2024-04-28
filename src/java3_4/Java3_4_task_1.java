3e0db00d9df28d4bab51b6cd13cb093499acdc75
User <User@DESKTOP-M2EEJ08.lan>
2024-04-28 00:12:56 -0600

    Java3_4_task_1 completed on 04/28/2024

---
+++ src/java3_4/Java3_4_task_1.java
@@ -0,0 +1,22 @@
+package java3_4;
+
+public class Java3_4_task_1 {
+
+	public static void main(String[] args) {
+		// while.
+		
+		// initialization.
+		int R=2;
+		
+		//condition.
+		while (R>=-10) {
+			System.out.println("R="+R);
+			
+			//decrement part.
+			R--;
+		}
+		
+
+	}
+
+}
