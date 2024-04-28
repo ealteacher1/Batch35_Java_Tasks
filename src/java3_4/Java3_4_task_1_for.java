f08fb69edc44f77822788ca667f5dc296cd0aae1
User <User@DESKTOP-M2EEJ08.lan>
2024-04-28 00:30:29 -0600

    Java3_4_task_1_for completed on 04/28/2024

---
+++ src/java3_4/Java3_4_task_1_for.java
@@ -0,0 +1,16 @@
+package java3_4;
+
+public class Java3_4_task_1_for {
+
+	public static void main(String[] args) {
+		//for.
+		for (int i=-5;i<5;i++) {
+			if(i==3) {
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
