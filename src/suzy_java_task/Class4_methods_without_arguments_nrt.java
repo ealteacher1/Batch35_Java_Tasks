a02dffc0125299122c83af66d612e7af5e2898c8
suzy8 <suzy8@10.0.0.176>
2024-04-29 19:43:54 -0700

    class4_task1_methods_without_argument_nrt_done by suzy 04/29/24

---
+++ src/suzy_java_task/Class4_methods_without_arguments_nrt.java
@@ -0,0 +1,72 @@
+package suzy_java_task;
+
+public class Class4_methods_without_arguments_nrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		addition();
+		subtraction();
+		multiplication();
+		if_else();
+		while_loop();
+
+	}
+//	Task1 
+//printing methods without arguments (no return type )
+	public static void addition () {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		
+	}	
+   public static void subtraction () {
+	   int a=20;
+	   int b=10;
+	   System.out.println(20-10);
+	   
+   }
+	public static void multiplication () {
+	 int a=20;
+	 int b=10;
+	 System.out.println(20*10);
+	 
+	}
+	 
+	 public static void if_else  () {
+	int  a=10;
+	int b=20 ;
+	if(a>b) {
+		System.out.println(true);
+	}
+	else {
+		System.out.println(false);
+	}
+	 }	
+	public static void while_loop() {
+		
+	int a=10;
+	String c="money";
+	while(a>=5) {
+		System.out.println(a+c);
+		a--;
+	}
+		
+	
+			
+	}	
+			
+	}
+	
+		 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+   
+		
+	
+
+
