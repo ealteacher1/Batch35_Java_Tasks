7af72d03bc10e97ba660db5b258f3156d4b61163
user <user@DESKTOP-GF39MDF.lan>
2024-04-30 16:53:16 -0500

    30/04/2024

---
+++ src/Shams_Demo/Task_2_Java_5.java
@@ -0,0 +1,51 @@
+package Shams_Demo;
+
+import Samia_Z.chartMetho;
+
+public class Task_2_Java_5 {
+
+	public static void main(String[] args) {
+		String str ="hello";
+		char ch='S';
+		double num =245.26;
+		int I=5698;
+		int O=26598;}
+	    String Method;
+		private int num;
+		chartMetho ch
+		doubleMethod;
+		interMethod ;
+		booleanMetho;
+
+  System.out.println("String method:"+str.to());
+	}
+  if(Character.isletter(ch)) {
+	  System.out.println("char method:It'sa letter");
+} else {
+	System.out.println(("char method:it's not a letter");
+	}
+  }
+  double result=num *2;
+	  System.out.println("double method:+result");
+	  public static void intMethod(int I) {
+		  int sum =I+O;
+		  int difference =O-I;
+		  System.out.println(("int Method-Sum:"+ sum +",Difference:"+difference);
+}
+	  
+  booleanMethod(boolean first,boolean method second){
+	  if (first && second) {
+		  System.out.println("Boolean method :one of them is true");
+	  }else {
+		  System.out.println("Boolean method : Both are false");
+	  }
+  }
+  
+  
+  
+	
+  
+
+	}
+
+}
