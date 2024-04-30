b3b0bc4aa565c9fa091d2d5593dd7ff60c887e06
syfur <syfur@10.0.0.136>
2024-04-30 01:52:06 -0400

    Class-4:Task-2 Created By Israt By 04/29/2024

---
+++ src/israts_java_task/Class_4_Task_2_Methods_MWA_NRT.java
@@ -0,0 +1,44 @@
+package israts_java_task;
+
+public class Class_4_Task_2_Methods_MWA_NRT {
+
+	public static void main(String[] args) {
+         dance("Dancing like a free bird ", "in style" );
+         hi('H');
+         hightime (59.20,12.30);
+         Umbrella(10,30);
+         Lowtime( 15.5f,5.5f);
+	}
+//Task-2-
+//Method With Arguments -Non Return Type 
+//with string	
+	public static void dance (String free,String style ) {
+		System.out.println(free+style );
+	}
+//with char	
+	public static void hi (char laugh ) {
+		System.out.println("The Character will be = "+ laugh);
+	}
+	
+//with double 
+	public static void hightime (double Double  ,double dine ) {
+		double eat=Double+dine;
+		System.out.println(eat +" is going to cost us for double dine  " );
+	}
+//with if -else
+     public static void Umbrella (int a,int b)	{
+    	 if (a>=b) {
+    		 System.out.println("life"+a);
+    	 }
+    	 else {
+    		 System.out.println("Life is beautiful ");
+    	 }
+    	 }
+//with double 
+ 	public static void Lowtime (float game   ,float zone  ) {
+ 	
+ 		System.out.println(game*zone);
+     }
+}
+
+
