775bac88c9b7b820fbadb3e1e4aec80e8810723b
user <user@DESKTOP-GF39MDF.lan>
2024-04-29 21:25:36 -0500

    task1class complited 29/04/2024

---
+++ src/Shams_Demo/Java_task1.java
@@ -0,0 +1,29 @@
+package Shams_Demo;
+
+public class Java_task1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		  int b=9;
+		  int c=120;
+		  System.out.println(a+b+c);
+		 
+		  int i = 200;
+		  String h= "samia";
+		  int e = 38;
+		  System.out.println(i+h+e);
+		  
+		  float f = 2.5f;
+		  double d= 145.265d;
+		 int g = 146;
+		 System.out.println(f+h+e);
+		 System.out.println(h+f+a) ;
+		 System.out.println(f+a+h);
+		 System.out.println(h+a+d+h);
+		 System.out.println(h+a+h+d+f);
+		 
+			}
+
+	}
+
+}
