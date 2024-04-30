07d6eaf65e330d1159fa9290e97dc115ae72a1bd
user <user@DESKTOP-GF39MDF.lan>
2024-04-29 21:34:59 -0500

    completed in 29/04/2024

---
+++ src/Shams_Demo/java_3_4_task1.java
@@ -0,0 +1,30 @@
+package Shams_Demo;
+
+public class java_3_4_task1 {
+
+	public static void main(String[] args) {
+		int a=2;
+		System.out.println("value of a is =a "+a);
+		a++;
+		
+		while(a>-10);
+		System.out.println(a);
+		
+		int b=3;
+		while(b<=20) {
+		System.out.println(b=+b);
+		++b;
+			
+		int c= -355;
+		for (c=355;c>=-255;c++) { 
+		System.out.println(c);
+		c++	;
+		}
+	}
+	}
+
+		
+
+	}
+
+}
