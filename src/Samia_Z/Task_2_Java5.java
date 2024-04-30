f9eb7baa49e52e3ccf82950eab3471918e5d8579
user <user@DESKTOP-GF39MDF.lan>
2024-04-30 16:50:00 -0500

    30/04/2024

--- src/Samia_Z/Task_2_Java5.java
+++ src/Samia_Z/Task_2_Java5.java
@@ -3,8 +3,43 @@ package Samia_Z;
 public class Task_2_Java5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String str ="hello";
+		char ch='S';
+		double num =245.26;
+		int I=5698;
+		int O=26598;}
+	    String Method;
+		private int num;
+		chartMetho ch
+		doubleMethod;
+		interMethod ;
+		booleanMetho;
 
+  System.out.println("String method:"+str.to());
 	}
-
+  if(Character.isletter(ch)) {
+	  System.out.println("char method:It'sa letter");
+} else {
+	System.out.println(("char method:it's not a letter");
+	}
+  }
+  double result=num *2;
+	  System.out.println("double method:+result");
+	  public static void intMethod(int I) {
+		  int sum =I+O;
+		  int difference =O-I;
+		  System.out.println(("int Method-Sum:"+ sum +",Difference:"+difference);
 }
+  booleanMethod(boolean first,boolean method second){
+	  if (first && second) {
+		  System.out.println("Boolean method :one of them is true");
+	  }else {
+		  System.out.println("Boolean method : Both are false"):
+	  }
+  }
+  }
+  }
+  
+	  }
+  }
