bf10f626a5aaaa45f63fb8b5e465460e6e0053f3
suzy8 <suzy8@10.0.0.176>
2024-04-29 22:34:38 -0700

    class4_task2_methods_with_argument_nrt_done by suzy 04/29/2024

---
+++ src/suzy_java_task/Class4_task2_methods_with_argument_nrt.java
@@ -0,0 +1,61 @@
+package suzy_java_task;
+
+public class Class4_task2_methods_with_argument_nrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		string ("i want joy" ," i want love" );
+		life('b');
+		party(20,30);
+		money(0.3f,0.1f);
+		honey(20,5);
+	}
+		
+//	Task2
+//	print methods with arguments(no return type )
+	
+//	string
+	public static void string (String joy, String love) {
+		System.out.println(joy+love);
+	}
+//	 char
+		public static void life(char moon) {
+			System.out.println("the sky is beautiful "+moon);
+			
+		}	
+//	double
+			public static void party(double person,double life) {
+				double sky =person*life ;
+				System.out.println("the sky is beautiful :"+sky);
+			}
+				
+//	float
+				public static void money(float big,float nice) {
+					float earn= big+nice ;
+					System.out.println("enjoy your big money " +earn);
+					
+				}
+// if_else 
+					public static void honey (int x,int y) {
+						if(x>=y) {
+							System.out.println("bee");
+						}
+						else {
+							System.out.println("bee make honey ");
+						}
+					
+				}
+			
+		
+	
+	
+	
+}
+		
+		
+	
+	
+
+	
+
+
