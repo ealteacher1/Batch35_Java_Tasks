02fef25fa5ae607cbc213116dc3f5a7c65aaae05
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-03 19:36:24 -0400

    may3rd,2024 task7

---
+++ src/taskHnifajava/Task7java.java
@@ -0,0 +1,88 @@
+package taskHnifajava;
+
+public class Task7java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+		Object number [][]= {{50, 60},{ 90, 100}};
+		
+		for (Object[] arr: number) {
+		for ( Object x: arr) {
+			
+			System.out.println(x);
+		}}
+			System.out.println("lenght of array" +  number.length);
+		}	
+	
+		
+}
+		
+		
+		
+		
+		/*Object  Save [][]={{ "Alice",123455667 }, {"Hnifa", 76544899 }};
+     
+	 for (Object[]arr1: Save ) {
+    	 for (Object x:arr1) {
+    		 
+    		 System.out.println(x);
+    	 }
+    	System.out.println(Save.length); 
+    	 0
+     }}}*/
+	
+		
+	/*	Object StringArray[][]= {{" Florida", 510 }, {"California", 603}};
+		
+	    for (int row=0;row<2; row++) {
+		for (int column=0; column<2; column ++) {
+			
+		System.out.println(StringArray[row][column]);
+		}
+		}
+	}
+	}*/
+
+    /*  int array2 [][]=new int  [2][2];
+   
+            array2[0][0]=50;
+
+             array2[0][1]=60;
+
+                 array2[1][0]=70;		
+
+                 array2[1][1]=80;		
+		
+		
+		
+		
+   for (int row=0;row<2; row++) {
+	for (int column=0; column<2; column++) {
+		
+	System.out.println(array2[row][column]);
+	
+	
+	
+	}}}}*/
+
+/* int array2 [][]=new int  [2][2];
+	   
+    array2[0][0]=50;
+
+     array2[0][1]=60;
+
+         array2[1][0]=70;		
+
+         array2[1][1]=80;		
+
+
+
+
+
+System.out.println(array2[0][1]);
+
+
+}}*/
