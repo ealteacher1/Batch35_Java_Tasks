279089b574de3193eed77af6ff40d1bc44469b88
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-03 18:32:30 -0400

    Task 1 java -5-3-24

---
+++ src/taskHnifajava/Task1.java
@@ -0,0 +1,45 @@
+package taskHnifajava;
+
+public class Task1 {
+
+
+
+		
+		
+		public static void main(String[] args) {
+			//This is a printing statement of int+int
+				int  a= 13;
+				int  b= 45;
+				int  d=83;
+				System.out.println(a+b+d);
+				//this is a printing statement of int+string+int;
+				//String
+				String v1="Welcome Back";
+				String w2= "Pearls";
+		        System.out.println(a+v1+b);
+			//this is a printing float+double+int;
+				float f=5.8f;
+				double e=999.999;
+				System.out.println(f+e+d);
+				//this is a printing statements of  String+float+int ;
+				System.out.println(v1+f+d);
+			//this is a printing statements of float+int+String ;
+				System.out.println(f+b+v1);
+			//this is a printing statements of Print float + int +float +double ;
+				System.out.println(f+b+f+e);
+			//this is printing statement of  String +int+double+String ;
+				System.out.println(v1+a+e+v1);
+			//this is a printing statements of  String+int+string+double
+				System.out.println(w2+b+v1+e);
+			//this is a printing statements of int+int+string+(int+int+int);
+				System.out.println(a+d+w2+(a+b+d));
+				
+				
+
+			}
+
+	
+
+
+	}
+
