f5e22b94a79dc177cde134d6c553b64bdc083865
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-03 19:02:11 -0400

    may-32024

---
+++ src/taskHnifajava/Arraytask1.java
@@ -0,0 +1,169 @@
+package taskHnifajava;
+
+public class Arraytask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	// question # 1
+	
+int score []  = new int[10];
+
+score[0]=11; 
+score[1]=6;
+score[2]=7;
+score[3]=2;
+score[4]=3;
+score[5]=1;
+score[6]=13;
+score[7]=11;
+score[8]=10;
+score[9]=5;
+
+
+  for (int x =0; x<score.length; x++ ) {
+   System.out.println(score[x]);	  
+  }
+  
+  
+  System.out.println(score[0]);
+  System.out.println(score[6]);
+  System.out.println(score[8]);
+  
+  
+
+System.out.println("length of array: "+ score.length);
+
+
+
+	
+	
+	
+	
+}
+} 
+	
+	
+	// For each  loop question #2
+/*	int Cars [] =new int [10];
+      Cars[0]=3;
+      Cars[1]=2;	
+      Cars[2]=8;
+      Cars[3]=20;
+      Cars[4]=2;
+      Cars[5]=5;
+      Cars[6]=43;
+      Cars[7]=8;
+      Cars[8]=90;
+      Cars[9]=34;
+      
+      for (int  x : Cars) {
+    	  
+      System.out.println(x);
+      
+      }
+      }}*/
+
+//for loop question #3
+/*   int Cars [] =new int [10];
+     Cars[0]=3;
+     Cars [1]=2;	
+     Cars[2]=8;
+     Cars[3]=20;
+     Cars[4]=2;
+     Cars[5]=5;
+     Cars[6]=43;
+     Cars[7]=8;
+     Cars[8]=90;
+     Cars[9]=34;
+
+for ( int j=0; j<Cars.length; j++) {
+System.out.println(Cars[j]);
+
+
+
+
+
+} 
+
+}}/*
+// question#4
+	
+	 /* int Cars [] =new int [10];
+	 Cars[4]=2;
+	 Cars[6]=43;
+	 System.out.println(Cars[4]);
+	 System.out.println(Cars[6]);
+	 
+}} */
+	
+	
+	//question #5
+
+/*	int Cars []= new int [10];
+	 Cars[0]=3;
+     Cars[1]=2;	
+     Cars[2]=8;
+     Cars[3]=20;
+     Cars[4]=2;
+     Cars[5]=5;
+     Cars[6]=43;
+     Cars[7]=8;
+     Cars[8]=90;
+     Cars[9]=34;
+     
+     for (int c=Cars.length-1;  c>=0; c--) {
+    System.out.println(Cars[c]);
+    
+     }
+     System.out.println("Length of Array  is: " + Cars.length);
+    
+     }
+     } */
+
+	// question #6
+/*  int  Cars []= new int [10];
+    Cars[0]=3;
+    Cars[1]=2;	
+    Cars[2]=8;
+    Cars[3]=20;
+    Cars[4]=2;
+    Cars[5]=5;
+    Cars[6]=43;
+    Cars[7]=8;
+    Cars[8]=90;
+    Cars[9]=34;
+    
+   
+    for( int h= 4;  h<=7; h++) {
+    	System.out.println(Cars[h]);
+    	
+    }
+     System.out.println(Cars.length );
+    }} */
+
+
+//question #7     
+/*	int  Cars []= new int [10];
+    
+	
+	Cars[0]=3;
+    Cars[1]=2;	
+    Cars[2]=8;
+    Cars[3]=20;
+    Cars[4]=2;
+    Cars[5]=5;
+    Cars[6]=43;
+    Cars[7]=8;
+    Cars[8]=90;
+    Cars[9]=34;
+    
+    
+    for (int i =7; i>=3; i--) {
+    	
+    	System.out.println(Cars[i]);
+    	
+    }
+       }} */
+
