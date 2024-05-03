46f73c880248360b52c1cb45ffb5b7b17cacb81b
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-03 19:14:51 -0400

    may 3rd ,2024forlooptask3

---
+++ src/taskHnifajava/Task3forloop.java
@@ -0,0 +1,33 @@
+package taskHnifajava;
+
+public class Task3forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	       //Do While loop,question #1
+		
+		/*   int w=2;
+		      do {
+		    	  System.out.println("Value of w is ="+ w);
+		    	  w--;
+		    	  
+		    	    }
+		      while (w>=-10);
+		}}*/
+
+	     
+			
+			// question # 3
+	   /*     int f=-355;
+	        do {
+	        	System.out.println("Value of f is ="+f);
+	        	f++;
+	        	
+	        }
+	        while (f<=-255);
+	        
+		} }*/
+
+	}}
