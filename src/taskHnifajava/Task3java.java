631ec34106d64b4663aa5d2867cf3c97acd517a0
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-03 19:27:03 -0400

    may3rd,2024,task3ofjava

---
+++ src/taskHnifajava/Task3java.java
@@ -0,0 +1,50 @@
+package taskHnifajava;
+
+public class Task3java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String  City  [] = { "Charlotte", "NewYork City"," Phoenix" , "Miami", "Orlando",};
+		for (String d: City ){
+			
+			System.out.println(d);
+			
+		}}}
+		
+   /* String  City  [] = { "Charlotte", "NewYork City"," Phoenix" , "Miami", "Orlando",};
+
+		
+		System.out.println(City[2]);
+		
+		System.out.println(City[3]);
+	}}*/
+		
+		 /*   String  City  [] = { "Charlotte", "NewYork City"," Phoenix" , "Miami", "Orlando",};
+
+	 	  
+          for (int c=0; c<=City.length-1; c++) {
+          
+        	 System.out.println(City[c]);	 
+         
+         }
+         }}*/
+		
+		/*String  City  [] = { "Charlotte", "NewYork City"," Phoenix" , "Miami", "Orlando",};
+
+	 	  
+        for (int c=City.length-1;  c>=0; c--) {
+       
+        	System.out.println(City[c]);	
+        }}}*/
+		
+       
+
+	/*String  City  [] = { "Charlotte", "NewYork City"," Phoenix" , "Miami", "Orlando",};
+	
+	
+	for (int c=4; c>1; c--) {
+	System.out.println(City[c]);	
+	
+	}}
+	}*/
