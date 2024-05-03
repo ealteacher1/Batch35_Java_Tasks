acd2f1fc57883af58785aad4d962e3a1adc479a0
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-03 19:19:44 -0400

    may 3rd ,forlooptask4

---
+++ src/taskHnifajava/forlooptask4.java
@@ -0,0 +1,24 @@
+package taskHnifajava;
+
+public class forlooptask4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+		  
+		// for loop, Question # 4
+	/*  for (int i=-956; i>=-960 ; i--) {
+		  System.out.println("The value of i="+i);
+	  }
+	}
+}*/
+     
+		// for loop question #7
+     for (int p=-500; p<=-200; p++) {
+    	 System.out.println("The value of p="+p);
+    	 if( p==-278) {
+    	 break;
+    	 }}}}
+     
+	
