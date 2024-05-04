b2bfdaa3f14b8fadbf4af77d80308c489d3525e0
Tahsin Nazmee <tnazmee@yahoo.com>
2024-05-04 12:29:18 -0500

    Updated and Completed Class 34 & 35 Task 6 on 5/4/2024.

--- src/Tahsin_Class_34and35_Tasks5to7/Class_D.java
+++ src/Tahsin_Class_34and35_Tasks5to7/Class_D.java
@@ -5,12 +5,18 @@ public class Class_D {
 	public static void main(String[] args) {
 		// Non-Static Method
 		Class_D CallingFromD1 = new Class_D();
+		CallingFromD1.methodD1();
 		Class_D CallingFromD2 = new Class_D(2);
+		CallingFromD2.methodD2(2);
 		Class_D CallingFromD3 = new Class_D("Example 3");
+		CallingFromD3.methodD3("Example 3");
 		Class_D CallingFromD4 = new Class_D("Example", "4");
+		CallingFromD4.methodD4(4, 4);
 		Class_D CallingFromD5 = new Class_D("Example", 5);
+		CallingFromD5.methodD5("Exmaple", 5);
 		
 		Class_E CallingFromE = new Class_E();
+		CallingFromE.methodE();
 	}
 	
 	// Constructor
@@ -27,7 +33,7 @@ public class Class_D {
 		System.out.println(addition);
 	}
 	
-	public Class_D(String ex3) {
+	public Class_D(String Ex3) {
 		int num = 25;
 		while(num<=30) {
 			System.out.println("The value is " + num);
@@ -51,6 +57,46 @@ public class Class_D {
 			for (Object Array:studentArray) {
 				System.out.println(Array);
 			}
+		}
+	}
+	
+	// Access Modifier
+	public void methodD1() {
+		String method = "method";
+		String D1 = "D1";
+		String methodD1 = method + D1;
+		System.out.println(methodD1);
+	}
+	
+	public void methodD2(int Ex2) {
+		int minus1 = 49;
+		int minus2 = 36;
+		int subtraction = minus1 -minus2;
+		System.out.println(subtraction);
+	}
+	
+	public void methodD3(String Ex3) {
+		int num = 30;
+		do {
+			System.out.println("The value is " + num);
+			num--;
+		} while (num>=25);
+	}
+	
+	public void methodD4(int ex4, int Ex4) {
+		int age1 = 25;
+		int age2 = 17;
+		if (age1 < age2) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+	
+	public void methodD5(Object ex5, Object Ex5) {
+		String colors[] = {"brown", "white", "yellow"};
+		for (String colorsArray:colors) {
+			System.out.println(colorsArray);
 		}
 	}
 	
--- src/Tahsin_Class_34and35_Tasks5to7/Class_E.java
+++ src/Tahsin_Class_34and35_Tasks5to7/Class_E.java
@@ -4,13 +4,19 @@ public class Class_E {
 
 	public static void main(String[] args) {
 		// Non-Static Method
-		Class_D CallingFromD = new Class_D();
+		Class_D CallingFromD1 = new Class_D();
+		CallingFromD1.methodD1();
 		Class_D CallingFromD2 = new Class_D(2);
+		CallingFromD2.methodD2(2);
 		Class_D CallingFromD3 = new Class_D("Example 3");
+		CallingFromD3.methodD3("Example 3");
 		Class_D CallingFromD4 = new Class_D("Example", "4");
+		CallingFromD4.methodD4(4, 4);
 		Class_D CallingFromD5 = new Class_D("Example", 5);
+		CallingFromD5.methodD5("Exmaple", 5);
 		
 		Class_E CallingFromE = new Class_E();
+		CallingFromE.methodE();
 	}
 	
 	// Constructor
@@ -18,6 +24,14 @@ public class Class_E {
 		String CallingFrom = "Calling From ";
 		String ClassName = "Class_E";
 		System.out.println(CallingFrom + ClassName);
+	}
+	
+	// Access Modifier
+	public void methodE() {
+		String method = "This method is ";
+		String nonstatic= "Nonstatic";
+		String nsmethod = method + nonstatic;
+		System.out.println(nsmethod);
 	}
 	
 	/* End */
