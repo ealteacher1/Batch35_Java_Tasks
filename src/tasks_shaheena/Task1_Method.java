0078a1d2c0184d0d42fd872030c5544ab48be2ed
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-05-05 14:51:53 -0400

    Task_1 Method  5-5-2024

---
+++ src/tasks_shaheena/Task1_Method.java
@@ -0,0 +1,107 @@
+package tasks_shaheena;
+
+public class Task1_Method {
+
+	public static void main(String[] args) {
+		
+         //	Create 5 Methods with  different Data type variables and Calulcations inside them and Call them in Main method.
+		//(You can use, addition, Subtraction, If else ,Print Statement etc)
+
+			
+		
+		    addition();
+			Subtraction();
+			Multiplication();
+			ifelse();
+			String();
+			division();
+			longnumber();
+		
+		}
+	      public  static void addition() {
+	    	    int a= 20;
+	    	    int b=30;
+	    	    int total= a+b;
+	    	    System.out.println("Result of Addition:=   " +total);
+	    	  
+	    	  
+	      }
+		
+		public static void Subtraction() {
+			
+			int c= 100;
+			int d = 70;
+			int x=c-d;
+			System.out.println( "Result of Subtraction:=  " +x);
+			
+		  }
+		
+		public static void Multiplication() {
+		 float e =4.5f;
+		 float f = 2.3f;
+		 float total = e*f;
+		 System.out.println("Result of multiplication := " +total);
+		 
+		 
+		 
+			
+		  }
+		
+		
+		
+		public static void division() {
+			double g = 7777.7777;
+			double h = 6666.6666;
+			double total = g/h;
+			System.out.println("Result of division:=  " + total);
+			
+			
+			
+			
+		}
+		
+		
+		public static void longnumber() {
+			long j = 1256789l;
+			long k = 12345667l;
+			long total= j+k;
+			System.out.println("total of  long:=  " + total);
+			
+			
+		}
+		  public static void ifelse() {
+			  
+			 int x=20;
+			 
+			  if (x>=30) { 
+				  System.out.println("Statement is false");
+			  }else {
+				  
+				  System.out.println("Statement is true");
+				  
+			  }
+
+
+				
+				
+				
+			} 
+		    public 	 static void String() {
+			
+		    String   firstname=" Shaheena";
+		    String   lastname= "Rahman";
+		
+		    String  fullname= firstname + lastname;
+			
+		    System.out.println("my fullname is: " + fullname);
+			
+		    }}
+			
+		
+		
+
+						
+				
+	
+
+
