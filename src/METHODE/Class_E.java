924f10c16f56042e3106c2c33607629c3e624cc6
User <User@DESKTOP-M2EEJ08.lan>
2024-05-06 21:56:21 -0600

    Class_E.java completed on 5/6/24

---
+++ src/METHODE/Class_E.java
@@ -0,0 +1,17 @@
+package METHODE;
+
+public class Class_E {
+
+	
+	    // Method to call constructors from Class_D
+	    public void callingFromE() {
+	        // Call all constructors from Class_D
+	        new Class_D();
+	        new Class_D(5);
+	        new Class_D("love");
+	        new Class_D(20, "daughter");
+	        new Class_D("mum", 1);
+	    }
+	}
+
+
