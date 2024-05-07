26fb867128a9c900a73148d29594ccee2f3fb2f3
User <User@DESKTOP-M2EEJ08.lan>
2024-05-06 22:06:39 -0600

    Access_modifier.java completed on 5/6/24

---
+++ src/METHODE/Access_modifier.java
@@ -0,0 +1,44 @@
+package METHODE;
+
+public class Access_modifier {
+
+	    // Public static method
+	    public static void publicStaticMethod() {
+	        System.out.println("Public static method called.");
+	    }
+	    
+	    // Public non-static method
+	    public void publicNonStaticMethod() {
+	        System.out.println("Public non-static method called.");
+	    }
+	    
+	    // Private static method
+	    private static void privateStaticMethod() {
+	        System.out.println("Private static method called.");
+	    }
+	    
+	    // Private non-static method
+	    private void privateNonStaticMethod() {
+	        System.out.println("Private non-static method called.");
+	    }
+	    
+	    // Protected static method
+	    protected static void protectedStaticMethod() {
+	        System.out.println("Protected static method called.");
+	    }
+	    
+	    // Protected non-static method
+	    protected void protectedNonStaticMethod() {
+	        System.out.println("Protected non-static method called.");
+	    }
+	    
+	    // Default static method
+	    static void defaultStaticMethod() {
+	        System.out.println("Default static method called.");
+	    }
+	    
+	    // Default non-static method
+	    void defaultNonStaticMethod() {
+	        System.out.println("Default non-static method called.");
+	    }
+	}
