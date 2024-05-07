806df376e87e6d6abba131cdf909f59d48d14b7d
Tahsin Nazmee <tnazmee@yahoo.com>
2024-05-07 14:00:38 -0500

    Updated and Completed Class 34 & 35 Task 5 on 5/7/2024.

--- src/Tahsin_Class_34and35_Tasks5to7/Class_A.java
+++ src/Tahsin_Class_34and35_Tasks5to7/Class_A.java
@@ -3,16 +3,22 @@ package Tahsin_Class_34and35_Tasks5to7;
 public class Class_A {
 
 	public static void main(String[] args) {
-		// Non-Static Method
+		// Calling Non-Static Methods
 		Class_A CallingFromA = new Class_A();
 		CallingFromA.Example1();
 		CallingFromA.Example2();
 		CallingFromA.Example3();
+		CallingFromA.Example4();
+		CallingFromA.Example5();
+		CallingFromA.Example6();
 		
 		Class_C CallingFromC = new Class_C();
-		CallingFromC.Example4();
-		CallingFromC.Example5();
-		CallingFromC.Example6();
+		CallingFromC.Example7();
+		CallingFromC.Example8();
+		CallingFromC.Example9();
+		CallingFromC.Example10();
+		CallingFromC.Example11();
+		CallingFromC.Example12();
 	}
 
 	// Constructor
@@ -22,6 +28,7 @@ public class Class_A {
 	}
 	
 	//Access Modifiers
+	// Integer One-Dimensional Array
 	public void Example1() {
 		int example1[] = {1, 2, 3};
 		for(int ex1:example1) {
@@ -29,33 +36,49 @@ public class Class_A {
 		}
 	}
 	
+	// String One-Dimensional Array
 	public void Example2() {
-		Object example2[][] = {{3, "cats"}, {2, "girls"}, {1, "boy"}};
-		for(Object[] ex2:example2) {
-			for(Object Ex2:ex2) {
-				System.out.println(Ex2);
+		String example2[] = {"Doc", "Bala", "Layla"};
+		for (String ex2:example2) {
+			System.out.println(ex2);
+		}
+	}
+	
+	// Object One-Dimensional Array
+	public void Example3() {
+		Object example3[] = {"Age", 25, true};
+		for (Object ex3:example3) {
+			System.out.println(ex3);
+		}
+	}
+	
+	// Integer Two-Dimensional Array
+	public void Example4() {
+		int example4[][] = {{3, 9}, {4, 16}};
+		for (int[] ex4:example4) {
+			for(int Ex4:ex4) {
+				System.out.println(Ex4);
 			}
 		}
 	}
 	
-	public void Example3() {
-		int day = 2;
-		switch (day) {
-			case 1:
-				System.out.println("Monday");
-				break;
-			case 2:
-				System.out.println("Tuesday");
-				break;
-			case 3:
-				System.out.println("Wednesday");
-				break;
-			case 4:
-				System.out.println("Thursday");
-				break;
-			case 5:
-				System.out.println("Friday");
-				break;
+	// String Two-Dimensional Array
+	public void Example5() {
+		String example5[][] = {{"male", "Tahsin"}, {"female", "Nishat"}};
+		for (String[] ex5:example5) {
+			for (String Ex5:ex5) {
+				System.out.println(Ex5);
+			}
+		}
+	}
+	
+	// Object Two-Dimensional Array
+	public void Example6() {
+		Object example6[][] = {{3, "cats"}, {2, "girls"}, {1, "boy"}};
+		for(Object[] ex6:example6) {
+			for(Object Ex6:ex6) {
+				System.out.println(Ex6);
+			}
 		}
 	}
 	
--- src/Tahsin_Class_34and35_Tasks5to7/Class_C.java
+++ src/Tahsin_Class_34and35_Tasks5to7/Class_C.java
@@ -3,16 +3,22 @@ package Tahsin_Class_34and35_Tasks5to7;
 public class Class_C {
 
 	public static void main(String[] args) {
-		// Non-Static Method
+		// Calling Non-Static Methods
 		Class_A CallingFromA = new Class_A();
 		CallingFromA.Example1();
 		CallingFromA.Example2();
 		CallingFromA.Example3();
+		CallingFromA.Example4();
+		CallingFromA.Example5();
+		CallingFromA.Example6();
 		
 		Class_C CallingFromC = new Class_C();
-		CallingFromC.Example4();
-		CallingFromC.Example5();
-		CallingFromC.Example6();
+		CallingFromC.Example7();
+		CallingFromC.Example8();
+		CallingFromC.Example9();
+		CallingFromC.Example10();
+		CallingFromC.Example11();
+		CallingFromC.Example12();
 	}
 	
 	// Constructor
@@ -21,29 +27,57 @@ public class Class_C {
 	}
 	
 	// Access Modifiers
-	public void Example4() {
-		Object example4[] = {'m', "brown", 25, true};
-		for(Object ex4:example4) {
-			System.out.println(ex4);
+	// Integer One-Dimensional Array
+	public void Example7() {
+		int example7[] = {4, 5, 6};
+		for (int ex7:example7) {
+			System.out.println(ex7);
 		}
 	}
 	
-	public void Example5() {
-		String example5[][] = {{"red", "apple"}, {"yellow", "banana"}, {"purple", "grape"}};
-		for(String[] ex5:example5) {
-			for(String Ex5:ex5){
-				System.out.println(Ex5);
+	// String One-Dimensional Array
+	public void Example8() {
+		String example8[] = {"Group Members", "Reyazul", "Israt", "Suzy", "Tahsin"};
+		for (String ex8:example8) {
+			System.out.println(ex8);
+		}
+	}
+	
+	// Object One-Dimensional Array
+	public void Example9() {
+		Object example9[] = {2, "males", 2, "females", true};
+		for(Object ex9:example9) {
+			System.out.println(ex9);
+		}
+	}
+	
+	// Integer Two-Dimensional Array
+	public void Example10() {
+		int example10[][] = {{5, 25}, {6, 36}};
+		for (int[] ex10:example10) {
+			for (int Ex10:ex10) {
+				System.out.println(Ex10);
 			}
 		}
 	}
 	
-	public void Example6() {
-		int a = 5;
-		int b = 22;
-		if (a<b) {
-			System.out.println("The value is " + true);
-		} else {
-			System.out.println("The value is " + false);
+	// String Two-Dimensional Array
+	public void Example11() {
+		String example11[][] = {{"red", "apple"}, {"yellow", "banana"}, {"purple", "grape"}};
+		for(String[] ex11:example11) {
+			for(String Ex11:ex11){
+				System.out.println(Ex11);
+			}
+		}
+	}
+	
+	// Object Two-Dimensional Array
+	public void Example12() {
+		Object example12[][] = {{3, "Hobbies"}, {"Basketball", true}, {"Working Out", true}, {"Soccer", false}};
+		for (Object[] ex12:example12) {
+			for (Object Ex12:ex12) {
+				System.out.println(Ex12);
+			}
 		}
 	}
 	
