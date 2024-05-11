64cfff269acb7307c1b420bb8bb8a6a899227487
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-05-11 15:57:12 -0400

    may 11,2024 method task 1

---
+++ src/methode/Methodetasks.java
@@ -0,0 +1,47 @@
+package methode;
+
+public class Methodetasks {
+
+	public static void main(String[] args) {
+		software1();
+		salary2();
+		condition3();
+		String();
+	  
+}
+	// This is task 1 
+	//Method without arguments (non return type )addition;
+	public static void software1 () {
+		int X =22;
+		int Y = 44;
+	System.out.println(X+Y);
+	}
+	
+	// Method Without arguments (non return type)subtraction;
+	public static void salary2 () {
+		int t=1000;
+		int l=2000;
+		System.out.println(l-t);
+	}
+	
+
+// Method without arguments (non return type) if else;
+     public static void condition3 () {
+    	int s=3000;
+    	 if (s> 4000) {
+    		 System.out.println("condition is true");
+    }else {
+    	     System.out.println("condition is false");
+    }}
+    	
+    	 
+ // Method without argument (non return type) Print statement;
+    	public static  void String() {
+    	      String flower="tulip";
+    	      String roses="rose";
+    	      String flowerbouquet= flower+roses; 
+    		 System.out.println("flowerbouquet");
+    	 
+    	 } 
+     }
+	
