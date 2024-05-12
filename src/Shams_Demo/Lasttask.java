55fe293e8f6e75bfd31225d787962084afb81b3e
HP <HP@DESKTOP-8I6N7R2.attlocal.net>
2024-05-12 12:50:20 -0700

    Method with argument,return type-Task o4,cmpleted on 05.12.2024

---
+++ src/Shams_Demo/Lasttask.java
@@ -0,0 +1,19 @@
+package Shams_Demo;
+
+public class Lasttask {
+
+	public static void main(String[] args) {
+		profit(3,23);
+
+		
+		
+		
+		
+	}
+	
+
+	public static int profit(int x,int y) {
+		int profit1=x+y;
+		return profit1;
+	}
+}
