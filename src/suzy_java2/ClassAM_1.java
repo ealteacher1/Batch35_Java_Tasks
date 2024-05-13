cb680d10cb75b24b05c35b3268f3d9263dabf9d1
suzy8 <suzy8@10.0.0.176>
2024-05-13 15:40:27 -0700

    class4_task7_done by suzy on 05/13/2024

---
+++ src/suzy_java2/ClassAM_1.java
@@ -0,0 +1,31 @@
+package suzy_java2;
+
+public class ClassAM_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		call all methods from Class_S (ClassAM_2 is from same package )  --
+		
+		 Class_S.method();
+		 Class_S.protected1 ();
+		 Class_S.default1();
+		 Class_S sun = new Class_S();
+		 sun.method1();
+		 Class_S pets =new Class_S();
+		 pets.nsprotected ();
+		 Class_S fun =new Class_S();
+	     Class_S job = new Class_S();
+		 job.nsdefault ();
+		 Class_S music =new Class_S();
+		 music.car();
+		 Class_S four = new Class_S();
+		 four.big ();
+		 Class_S rain =new Class_S();
+		 Class_S son = new Class_S();
+		 son.far();
+	}
+}
+
+
---
+++ src/suzy_java_task/ClassAM_2.java
@@ -0,0 +1,25 @@
+package suzy_java_task;
+
+import suzy_java2.Class_S;
+
+public class ClassAM_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		call all methods from Class_S (ClassAM_2 is from different package )  --
+		
+		
+		 Class_S.method();
+		 Class_S sun = new Class_S();
+		 sun.method1();
+		 Class_S music =new Class_S();
+		 music.car();
+		
+		
+	}
+
+
+	}
+
+
