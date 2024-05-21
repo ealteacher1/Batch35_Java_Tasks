30b6dd91c6321539c63f1133dfac3fd532024d51
HP <HP@DESKTOP-8I6N7R2.attlocal.net>
2024-05-21 09:37:25 -0700

    Task_05.Java.completed 05/20/2024

---
+++ src/Shams_Demo/ClassCC.java
@@ -0,0 +1,25 @@
+package Shams_Demo;
+
+public class ClassCC {
+
+	public static void main(String[] args) {
+		 ClassA ob3=new  ClassA();
+		 ob3.sam();
+         ob3.sam1();
+		  ob3.sam1();
+		  ob3.sam2();
+		  ob3.sam2("ss", "dd");
+		  ob3.sam33();
+		  ob3.sam4(3, 2);
+		  ob3.sam5(3, 3);
+		  ob3.samira1();
+		ob3.samira2(3, 4);
+		ob3.samira3();
+		ob3.samira4("AA", "CC");
+		
+		
+	}
+     public void CallingfromC(int x,int y) {
+    	 System.out.println(x+y);
+     }
+}
