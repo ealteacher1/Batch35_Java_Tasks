fa4e734b27316b2df54c429cfffc38fdf65be1c2
Tahsin Nazmee <tnazmee@yahoo.com>
2024-05-27 18:02:51 -0500

    Completed Class 37 to 40 Task 5 on 5/27/2024.

---
+++ src/Tahsin_Class_37to40_Tasks1to5/Task5_A.java
@@ -0,0 +1,25 @@
+package Tahsin_Class_37to40_Tasks1to5;
+
+public class Task5_A {
+	
+	private int age;
+	private String name;
+	
+	// Getters and Setters
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/Tahsin_Class_37to40_Tasks1to5/Task5_B.java
@@ -0,0 +1,17 @@
+package Tahsin_Class_37to40_Tasks1to5;
+
+public class Task5_B {
+
+	public static void main(String[] args) {
+		// Calling From Task5_A
+		Task5_A ob = new Task5_A();
+		ob.setAge(25);
+		int Age = ob.getAge();
+		System.out.println(Age);
+		
+		ob.setName("Tahsin Nazmee");
+		String Name = ob.getName();
+		System.out.println(Name); 
+	}
+
+}
