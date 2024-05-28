cfe8aed8e701f46b573858de924e2ef58c1906b0
suzy8 <suzy8@suzy>
2024-05-28 11:43:57 -0700

    Class 73_task 1_done by suzy on 05/28/2024

--- src/suzy_java3/Suzy_task1.java
+++ src/suzy_java3/Suzy_task1.java
@@ -8,32 +8,30 @@ public class Suzy_task1 {
 		ob.family(ob);  //calling by variable 
 	    ob.family(new Suzy_task1());    //calling by value 
 	    
+	    
 	    Suzy_task1 ob1 = new Suzy_task1 ();
 	    ob1.home (ob1);   //calling by variable 
 	    ob.home(new Suzy_task1());   //calling by value 
 	    
+	    
 	     Suzy_task1 ob2 = new Suzy_task1 ();
-	     ob2.kids(ob2);
-	     ob2.kids(new Suzy_task1()) ;
+	     ob2.kids(ob2);   //calling by variable 
+	     ob2.kids(new Suzy_task1()) ;   //calling by value
+	     
 	     
 	     Suzy_task1 ob3 = new  Suzy_task1 ();
-	     ob3.boy (ob3);
-	     ob3 .boy ( new Suzy_task1 ());
+	     ob3.boy (ob3);   //calling by variable 
+	     ob3 .boy ( new Suzy_task1 ());   //calling by value
+	     
 	     
 	     Suzy_task1 ob4 = new Suzy_task1 ();
-	     ob4.girl(ob4);
-	     ob4.girl(new Suzy_task1());
-	     
-	     
-	     
-	     
-	     
+	     ob4.girl(ob4);   //calling by variable 
+	     ob4.girl(new Suzy_task1());   //calling by value
 	     
 	
 	
-    
-		
-//		create 5 examples by of calling by object reference /class variable --
+  
+//	create 5 examples by of calling by object reference /class variable --
 		
 	}
 	public void family ( Suzy_task1 one) {
