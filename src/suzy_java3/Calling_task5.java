a5de92057ffbbeeb17a1d4b4eb28812007814641
suzy8 <suzy8@suzy>
2024-06-06 19:59:15 -0700

    Class37-40 task_5 done by Suzy on 06/06/2024

---
+++ src/suzy_java3/Calling_task5.java
@@ -0,0 +1,30 @@
+package suzy_java3;
+
+public class Calling_task5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Calling from class5 --
+		
+		Task_5 ob =new Task_5 ();
+		  ob.setX(10);
+		  int boy =ob.getX();
+		  System.out.println(boy);
+		
+		  
+		 Task_5 ob1 =new Task_5 ();
+		 ob1.setY(300);
+		 int girl = ob.getY() ;
+		 System.out.println(girl);
+		
+	}
+
+	
+		
+	}
+
+	
+	
+
+
---
+++ src/suzy_java3/Task_5.java
@@ -0,0 +1,38 @@
+
+package suzy_java3;
+
+public class Task_5 {
+
+
+		
+		
+//		Create 2 global variables --
+		
+		private int x ;
+		private int y ;
+
+		public static void main(String[] args) {
+		
+			Task_5 ob =new Task_5 ();
+			
+//       generate getter and setter --
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+}
+
+	
