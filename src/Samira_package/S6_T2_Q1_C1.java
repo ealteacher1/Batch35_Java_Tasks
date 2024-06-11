62c5771462ff5e73d2d1b383747161257afa7090
HP <HP@192.168.1.76>
2024-06-11 14:43:47 -0700

    Task 02/ Class 42/43/Java 06. completed on 06/11/2024

---
+++ src/Samira_package/S6_T2_Q1_C1.java
@@ -0,0 +1,24 @@
+package Samira_package;
+
+public class S6_T2_Q1_C1 {
+
+	
+	
+	static int x=1;
+	public static void main(String[] args) {
+		System.out.println(x);
+	}
+     public static void c11() {
+    	 
+    	 
+     }
+     public void A() {
+    	 System.out.println("A from C1");
+     }
+    
+    	 
+     
+   
+    	 
+     }
+
---
+++ src/Samira_package/S6_T2_Test.java
@@ -0,0 +1,16 @@
+package Samira_package;
+
+public class S6_T2_Test {
+
+	public static void main(String[] args) {
+		 S6_T2_Q1_C3 ob1= new  S6_T2_Q1_C3();
+		 ob1.d();
+		 ob1.f();
+		 ob1.h();
+		 ob1.A();
+		 ob1.C33();
+		 
+
+	}
+
+}
