0dccb3030bfd400ff4f3bf4b9445d951f97a515a
HP <HP@192.168.1.76>
2024-06-15 10:21:17 -0700

    completed on.06/15.2024

---
+++ src/Samira_package/Abstarct_1.java
@@ -0,0 +1,10 @@
+package Samira_package;
+
+public abstract class Abstarct_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Samira_package/Abstract_2.java
@@ -0,0 +1,10 @@
+package Samira_package;
+
+public abstract class Abstract_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
