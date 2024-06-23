c81d25c5c20fa61bcbf53f936272fc2433b9b065
smokh <smokh@DESKTOP-EL6J59M.attlocal.net>
2024-06-22 23:36:08 -0400

    java_6_task_2  06/22/2024

---
+++ src/taskHnifajava/S6_T2_Q1_C1.java
@@ -0,0 +1,19 @@
+package taskHnifajava;
+
+//Grand parent of C3
+public class S6_T2_Q1_C1 {
+	
+	
+	public void  Honesty() {
+		
+		System.out.println("Grandparent-voice-C1");
+	}
+  public void complexion() {
+  	
+  	System.out.println("Grandparent-Complexion-C1");
+  }
+   public void Height() {
+  	 
+  	 System.out.println("Grandparent-Height-C1");
+   }
+}
---
+++ src/taskHnifajava/S6_T2_Q1_C2.java
@@ -0,0 +1,44 @@
+package taskHnifajava;
+
+
+
+
+//Parent of C3, 
+
+
+public class S6_T2_Q1_C2   extends S6_T2_Q1_C1{
+	
+	
+
+	   //c. Create 2 overridden methods from parent Class .
+	
+	public void communication() {
+	
+	System.out.println("Parent-C2");
+	}
+
+	
+	public void intelligence() {
+ 	
+ 	System.out.println("Parent-C2");
+ }
+ 
+
+	
+	public void  patience() {
+ 	
+ 	System.out.println("Parent-C2");
+ }
+	
+	
+ public void Height() {      //overridden method
+ 	 
+	  System.out.println("C2-Parent inherits from Grand parent C1");
+ }
+
+
+ public void complexion() {  //overridden method 
+    	
+    	System.out.println("C2-Parent inherits from Grandparent C1");
+    
+}}
