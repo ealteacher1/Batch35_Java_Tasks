b13ba62b1c954ae9a0ecf52c52fe37f6d829d10e
HP <HP@DESKTOP-8I6N7R2.attlocal.net>
2024-07-06 20:21:31 -0700

    completed on 07.06.2024

---
+++ src/Revision/Wrapper.java
@@ -0,0 +1,35 @@
+package Revision;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		//Task 05_Java_46_47
+		
+		
+		// convert double to double object
+		double a= 12.22;
+		
+		Double ob= new Double(a);
+		System.out.println(ob);
+		
+		//Convert char'#' to character object
+		
+		char b='#';
+		Character ob1= new Character(b);
+        System.out.println(ob1);
+        
+        //Convert int 500 to Integer object
+        
+        int c=500;
+        Integer ob2=new Integer(c);
+        System.out.println(ob2);
+        
+        //Convert boolean false to boolean object
+        boolean d=false;
+        Boolean ob3=new Boolean(d);
+        System.out.println(ob3);
+        
+        
+	}
+
+}
