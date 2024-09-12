b9b3ffa2dfd65c4ffac5ff0c190ffdc7c4ccac8c
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-09-12 18:38:37 -0400

    Java#6 Task 6 Abstract method

---
+++ src/tasks_shaheena/Dolphin.java
@@ -0,0 +1,40 @@
+package tasks_shaheena;
+
+
+
+
+      //Subclass (inherits from Animal-paent class)
+
+        public abstract class Dolphin extends Animal{
+        	
+        	
+      //Abstract method doesn't have body  	
+        
+       public abstract  void sleep();
+       
+       
+	  //abstract method with parameters 
+	   public abstract void makeSound(String s, String t);
+	  
+	   
+	   //regular method 
+	   public  void eat() {
+	  System.out.println("Dolphin  eats fish");
+	  
+	   }
+	  
+  
+	  
+        
+      //Regular method
+	   public  void makesound(){
+	    System.out.println("weeeeeeeee");
+	  
+	  
+	  }
+   
+	
+	
+		  }
+		
+
