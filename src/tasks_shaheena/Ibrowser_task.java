e3f103e21c56a236702e90503608c929e767adb6
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-09-13 21:05:36 -0400

    Classv48- sel 1

---
+++ src/tasks_shaheena/Ibrowser_task.java
@@ -0,0 +1,59 @@
+package tasks_shaheena;
+
+/*import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Ibrowser_task {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+	
+	
+
+		
+			/*make global variable static, so that we can use this variablw directly in main method 
+			 
+			static WebDriver driver;
+
+		 public static void main(String[] args){
+		//setting driver properties: we show jvm where we kept our browser driver
+		//this key is String datatype key for chrome driver, full location path of the driver //paste it as a String within double quotation
+			 //close statement with semicolon 
+		System.setProperty("WebDriver.chrome.driver", "C:\\Users\\Shaheena Rahman\\eclipse-workspace\\Java -classwork\\project.selenium\\Drivers\\chromedriver.exe");
+		   
+
+		//write new chrome, ctl space
+		//to call chrome driver constructor
+		//creating object of chrome driver class
+		// when we write driver all methods related to chromedriver  class + webdriver interface will be displaying 
+		 
+		    driver= new ChromeDriver();	
+		    
+		    //get method to open url or website 
+		     
+		     
+		    //paste the url inside get method 
+		    //search amazon .com in browser , cpy. paste full url link
+		    driver.get("https://amazon.com");
+		    Thread.sleep(6000);
+		    driver.get("https://Google.com");
+		    Thread.sleep(6000);
+		    driver.get("https://www.facebook.com/");
+		    Thread.sleep(6000);
+		    driver.get("https://www.youtube.com/");
+		    Thread.sleep(6000);
+		    driver.get("https://www.ebay.com/");
+		    Thread.sleep(6000);
+		    		
+
+			}
+
+		
+
+		
+
+	}*/
+
+
